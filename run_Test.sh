515e82af0f47413a52513231028412ca8e317b93
Dileep Naik <DN@Dummy-MacBook.local>
2021-10-10 12:25:12 -0700

    buid command

--- run_Test.sh
+++ run_Test.sh
@@ -3,7 +3,9 @@
 rm -rfv ./reports/testCheck
 mkdir -p .reports/testCheck
 
-docker build --pull --no-cache -t katalonstudio/katalon .
+//docker build --pull --no-cache -t katalonstudio/katalon .
+
+docker build
 
 echo "dcoker build completed"
 
